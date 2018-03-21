e62d9969c13638e57b45ae786da3357ce4acedaf
Stephen Balogh <sgbalogh@gmail.com>
2018-03-21 15:33:06 -0400

    better handling of capitalization

--- wnlu/translate/WinogradSchema.py
+++ wnlu/translate/WinogradSchema.py
@@ -10,7 +10,24 @@ class WinogradSchema:
         return self.premise_A + " " + self.pronoun + " " + self.premise_B
 
     def get_translations(self):
-        return [
-            self.premise_A + " " + str.lower(self.answers[0]) + " " + self.premise_B,
-            self.premise_A + " " + str.lower(self.answers[1]) + " " + self.premise_B
-        ]
+        if (WinogradSchema.is_complete_sentence(self.premise_A)):
+            return [
+                self.premise_A + " " + self.answers[0] + " " + self.premise_B,
+                self.premise_A + " " + self.answers[1] + " " + self.premise_B
+            ]
+        else:
+            return [
+                self.premise_A + " " + str.lower(self.answers[0]) + " " + self.premise_B,
+                self.premise_A + " " + str.lower(self.answers[1]) + " " + self.premise_B
+            ]
+
+    @staticmethod
+    def is_complete_sentence(input):
+        cleaned = str.strip(input)
+        if len(cleaned) == 0:
+            return False
+        else:
+            if cleaned[-1] == '.':
+                return True
+            else:
+                return False
