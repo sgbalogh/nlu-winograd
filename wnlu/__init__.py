b93a49ac3855d48daae929574ec4d98eaab6ba79
Stephen Balogh <sgbalogh@gmail.com>
2018-03-28 11:18:32 -0400

    small API changes

--- wnlu/__init__.py
+++ wnlu/__init__.py
@@ -1,3 +1,3 @@
 from wnlu.translate.WinogradSchema import WinogradSchema
-from wnlu.translate.WinogradTranslator import WinogradTranslator
+from wnlu.translate.WinogradLoader import WinogradLoader
 from wnlu.translate.Utils import Utils
--- wnlu/translate/WinogradSchema.py
+++ wnlu/translate/WinogradSchema.py
@@ -1,29 +1,29 @@
 from wnlu.translate.Utils import Utils
 
+####
+## This class provides an encapsulization for a schema, which is characterized by:
+## 1) A "premise" string, containing a sentence (or potentially sequence of sentences).
+## 2) A reference to an ambiguous pronoun within the premise.
+## 3) Two potential noun options for that ambigous pronoun.
+## For labeled instances, there is also an index to which noun option correctly removes the
+## ambiguity in the premise.
+####
+
 class WinogradSchema:
-    def __init__(self, premise_A, pron, premise_B, answer_list, correct_answer):
+    def __init__(self, premise_A, pron, premise_B, answer_list, correct_answer=None):
         self.premise_A = str.strip(premise_A)
         self.pronoun = str.lower(str.strip(pron))
         self.premise_B = str.strip(premise_B)
         self.answers = list(map(lambda x: Utils.standardize_noun_phrase(str.strip(x)), answer_list))
         self.gold_answer_idx = correct_answer
 
+    ## Returns the original "premise" string, containing the pronoun in question
     def get_premise(self):
         return self.premise_A + " " + self.pronoun + " " + self.premise_B
 
-    # def get_translations_old(self):
-    #     if (Utils.is_complete_sentence(self.premise_A)):
-    #         return [
-    #             self.premise_A + " " + Utils.capitalize_beginning(self.answers[0]) + " " + self.premise_B,
-    #             self.premise_A + " " + Utils.capitalize_beginning(self.answers[1]) + " " + self.premise_B
-    #         ]
-    #     else:
-    #         return [
-    #             self.premise_A + " " + self.answers[0] + " " + self.premise_B,
-    #             self.premise_A + " " + self.answers[1] + " " + self.premise_B
-    #         ]
-
-    def get_translations(self):
+    ## Returns versions of the premise string with the candidate answers swapped-
+    ## in for the ambiguous pronoun
+    def get_candidate_translations(self):
         translations = []
         for potential_answer in self.answers:
             pa = potential_answer
