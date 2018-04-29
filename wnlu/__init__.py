b6115f8ea273a92a47a7cfbd1e65c1d2d43fb855
wesley-leong <37635009+wesley-leong@users.noreply.github.com>
2018-04-29 15:55:12 -0400

    Update __init__.py

--- wnlu/__init__.py
+++ wnlu/__init__.py
@@ -1,4 +1,3 @@
 from wnlu.translate.WinogradSchema import WinogradSchema
 from wnlu.translate.WinogradLoader import WinogradLoader
 from wnlu.translate.Utils import Utils
-from wnlu.translate.SentenceVariants import SentenceVariants
